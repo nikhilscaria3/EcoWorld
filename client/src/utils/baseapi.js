b5794fb8c988e017a26a4f7785ad15485b86b1e3
nikhilscaria3 <125331845+nikhilscaria3@users.noreply.github.com>
2023-10-19 09:24:17 +0530

    Update baseapi.js

--- client/src/utils/baseapi.js
+++ client/src/utils/baseapi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a new Axios instance with your custom configuration
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // Set your base URL here
+  baseURL: 'https://eco-world-server.vercel.app', // Set your base URL here
   headers: {
     'Content-Type': 'application/json',
     // You can add other default headers here
